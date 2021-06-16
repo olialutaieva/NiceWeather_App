1dc207ffa9b6e7f0d6f41417f3251e3a500742bb
Olia <olialutaeva@gmail.com>
2021-06-16 14:23:24 +0300

    First Commit

---
+++ app/src/main/java/com/example/niceweather/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.niceweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView MainLogo;
+    private LoginButton FacebookLoginBtn;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainLogo = findViewById(R.id.imageView);
+        FacebookLoginBtn = findViewById(R.id.login_facebook);
+        callbackManager = CallbackManager.Factory.create();
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        FacebookLoginBtn.setOnClickListener(v -> {
+            Collection<String> permissionNeeds= Arrays.asList("email");
+            LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, permissionNeeds);
+        });
+
+        FacebookLoginBtn.setPermissions(Arrays.asList("email", "user_birthday"));
+        FacebookLoginBtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent intent = new Intent(MainActivity.this, UIActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Вы отменили авторизацию.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "При авторизации произошла ошибка: " + error.toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/niceweather/UIActivity.java
@@ -0,0 +1,14 @@
+package com.example.niceweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uiactivity);
+    }
+}
